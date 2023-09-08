106451b82214ebc8a6d8e9ee6408bc74919895b1
Hrishabh963 <hrishabhkumar634@gmail.com>
2023-09-08 14:16:39 +0530

    Deleted redundant files

--- test/testProblem1.js
+++ test/testProblem1.js
@@ -7,61 +7,61 @@ const data = JSON.stringify({ Name: 'Hrishabh' });
 //Using callback only
 
 fsUsingCallback.callBackCreate(data, (error) => {
-    if (error) {
+  if (error) {
+    console.log(`Error encountered: ${error}`);
+    return;
+  } else {
+    fsUsingCallback.callBackDelete((error) => {
+      if (error) {
         console.log(`Error encountered: ${error}`);
         return;
-    } else {
-        fsUsingCallback.callBackDelete((error) => {
-            if (error) {
-                console.log(`Error encountered: ${error}`);
-                return;
-            } else {
-                console.log(`All files Deleted using callbacks`);
-            }
-        });
-    }
+      } else {
+        console.log(`All files Deleted using callbacks`);
+      }
+    });
+  }
 });
 
 //Using Promises only
 
 fsUsingPromise
-    .promiseMakeDir('test1_files')
-    .then(() => {
-        return fsUsingPromise.promiseCreate(data);
-    })
-    .then(() => {
-        return fsUsingPromise.promiseDelete();
-    })
-    .then(() => {
-        return fsUsingPromise.promiseRemoveDir('test1_files');
-    })
-    .catch((error) => {
-        if (error) {
-            console.log(error);
-        }
-    });
+  .promiseMakeDir('test1_files')
+  .then(() => {
+    return fsUsingPromise.promiseCreate(data);
+  })
+  .then(() => {
+    return fsUsingPromise.promiseDelete();
+  })
+  .then(() => {
+    return fsUsingPromise.promiseRemoveDir('test1_files');
+  })
+  .catch((error) => {
+    if (error) {
+      console.log(error);
+    }
+  });
 
 //Using Async/Await
 
 async function createDeleteUsingAsyncAwait() {
-    try {
-        await fsUsingAsyncAwait.asyncMakeDir('test1_files');
-        await fsUsingAsyncAwait.asyncAwaitCreate('file1.json', data);
-        await fsUsingAsyncAwait.asyncAwaitCreate('file2.json', data);
-        await fsUsingAsyncAwait.asyncAwaitCreate('file3.json', data);
-        console.log(`All files created using async/await`);
-        setTimeout(async() => {
-            await fsUsingAsyncAwait.asyncAwaitDelete('file1.json');
-            await fsUsingAsyncAwait.asyncAwaitDelete('file2.json');
-            await fsUsingAsyncAwait.asyncAwaitDelete('file3.json');
-            console.log(`All files deleted using async/await`);
-            await fsUsingAsyncAwait.asyncRemoveDir('test1_files');
-        }, 4000);
-    } catch (error) {
-        if (error) {
-            console.log(error);
-        }
+  try {
+    await fsUsingAsyncAwait.asyncMakeDir('test1_files');
+    await fsUsingAsyncAwait.asyncAwaitCreate('file1.json', data);
+    await fsUsingAsyncAwait.asyncAwaitCreate('file2.json', data);
+    await fsUsingAsyncAwait.asyncAwaitCreate('file3.json', data);
+    console.log(`All files created using async/await`);
+    setTimeout(async () => {
+      await fsUsingAsyncAwait.asyncAwaitDelete('file1.json');
+      await fsUsingAsyncAwait.asyncAwaitDelete('file2.json');
+      await fsUsingAsyncAwait.asyncAwaitDelete('file3.json');
+      console.log(`All files deleted using async/await`);
+      await fsUsingAsyncAwait.asyncRemoveDir('test1_files');
+    }, 4000);
+  } catch (error) {
+    if (error) {
+      console.log(error);
     }
+  }
 }
 
 createDeleteUsingAsyncAwait();
--- test/testProblem2.js
+++ test/testProblem2.js
@@ -3,12 +3,13 @@ const main = async() => {
     try {
         const uppperCaseFile = await problem2.makeContentUpperCase('lipsum.txt');
         await problem2.appendFileContent(`${uppperCaseFile}`);
-        const sentenceNames = await problem2.makeContentLowerCaseAndSplit(uppperCaseFile);
+        const sentenceNames =
+            await problem2.makeContentLowerCaseAndSplit(uppperCaseFile);
         await Promise.all(
             sentenceNames.map(async(sentenceName) => {
                 await problem2.appendFileContent(`\n${sentenceName}`);
-            })
-        )
+            }),
+        );
         const sortedFileName = await problem2.readContentAndSort(sentenceNames);
         await problem2.appendFileContent(`\n${sortedFileName}`);
         const fileNames = await problem2.readFileContent('filenames.txt');
@@ -17,12 +18,11 @@ const main = async() => {
             fileNamesArray.map(async(fileName) => {
                 await problem2.removeFile(fileName);
             })
-        )
-        await problem2.removeFile('filenames.txt')
+        );
+        await problem2.removeFile('filenames.txt');
         console.log(`All tasks completed successfully`);
-
     } catch (error) {
         console.log(error);
     }
-}
+};
 main();
